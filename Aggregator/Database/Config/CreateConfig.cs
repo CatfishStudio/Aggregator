22769cce1a544f79510c0a7f98a169b38534abb9
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2017-02-26 13:56:54 +0300

    Отдельный класс создания таблиц в config.mdb

--- Aggregator/Database/Config/CreateConfig.cs
+++ Aggregator/Database/Config/CreateConfig.cs
@@ -30,48 +30,7 @@ namespace Aggregator.Database.Config
 				Application.Exit();
 			}
 			
-			/* Создание таблицы пользователей */
-			CreateTable createTable;
-			createTable = new CreateTable("Users", DataConfig.configFile, DataConstants.TYPE_OLEDB);
-			createTable.СolumnAdd("ID", true, "COUNTER");
-			createTable.СolumnAdd("Name");
-			createTable.СolumnAdd("Pass");
-			createTable.СolumnAdd("Permissions");
-			try{
-				createTable.Execute();
-				createTable.InsertValue("0, 'Администратор', '', 'admin'");
-				createTable.InsertValue("1, 'Пользователь', '', 'user'");
-			}catch(Exception ex){
-				createTable.Error();
-				MessageBox.Show(ex.ToString(), "Ошибка:");
-				Application.Exit();
-			}
-			
-			DataConfig.localDatabase = DataConfig.resource + "\\database.mdb";
-			DataConfig.typeConnection = DataConstants.CONNETION_LOCAL;
-			
-			/* Создание таблицы настроек */
-			createTable = new CreateTable("Settings", DataConfig.configFile, DataConstants.TYPE_OLEDB);
-			createTable.СolumnAdd("ID", true, "COUNTER");
-			createTable.СolumnAdd("name");
-			createTable.СolumnAdd("localDatabase");
-			createTable.СolumnAdd("typeDatabase");
-			createTable.СolumnAdd("typeConnection");
-			createTable.СolumnAdd("server");
-			createTable.СolumnAdd("serverUser");
-			createTable.СolumnAdd("serverDatabase");
-			
-			try{
-				createTable.Execute();
-				createTable.InsertValue("0, 'database', '" + DataConfig.localDatabase 
-				                        + "', '" + DataConstants.TYPE_OLEDB 
-				                        + "', '" + DataConfig.typeConnection 
-				                        + "', 'localhost', 'sa', 'database'"); 
-			}catch(Exception ex){
-				createTable.Error();
-				MessageBox.Show(ex.ToString(), "Ошибка:");
-				Application.Exit();
-			}
+			CreateConfigTables.Create();
 		}
 	}
 }
---
+++ Aggregator/Database/Config/CreateConfigTables.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Cartish
+ * Date: 26.02.2017
+ * Time: 12:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Data.OleDb;
+using Aggregator.Data;
+using Aggregator.Database.Local;
+
+namespace Aggregator.Database.Config
+{
+	/// <summary>
+	/// Description of CreateConfigTables.
+	/// </summary>
+	public static class CreateConfigTables
+	{
+		public static void Create()
+		{
+			String sqlCommand;
+			QueryOleDb query;
+			query = new QueryOleDb(DataConfig.configFile);
+			
+			/* Таблица USERS */			
+			sqlCommand = "CREATE TABLE Users (" +
+				"[ID] COUNTER PRIMARY KEY, " +
+				"[Name] VARCHAR DEFAULT '' UNIQUE, " +
+				"[Pass] VARCHAR DEFAULT '', " +
+				"[Permissions] VARCHAR DEFAULT 'user'" +
+				")";
+			query.SetCommand(sqlCommand);
+			query.Execute();
+			
+			sqlCommand = "INSERT INTO Users (" +
+				"[Name], [Pass], [Permissions]) " +
+				"VALUES ('Администратор', '', 'admin')";
+			query.SetCommand(sqlCommand);
+			query.Execute();
+			
+			sqlCommand = "INSERT INTO Users (" +
+				"[Name], [Pass], [Permissions]) " +
+				"VALUES ('Пользователь', '', 'user')";
+			query.SetCommand(sqlCommand);
+			query.Execute();
+			
+			/* Таблица SETTINGS */
+			DataConfig.localDatabase = DataConfig.resource + "\\database.mdb";
+			DataConfig.typeConnection = DataConstants.CONNETION_LOCAL;
+			DataConfig.typeDatabase = DataConstants.TYPE_OLEDB;
+			DataConfig.server = "localhost";
+			DataConfig.serverUser = "sa";
+			DataConfig.serverDatabase = "database";
+			
+			sqlCommand = "CREATE TABLE Settings (" +
+				"[ID] COUNTER PRIMARY KEY, " +
+				"name VARCHAR(50) DEFAULT '' UNIQUE, " +
+				"localDatabase TEXT DEFAULT '', " +
+				"typeDatabase VARCHAR(50) DEFAULT '', " +
+				"typeConnection VARCHAR(50) DEFAULT '', " +
+				"server VARCHAR(255) DEFAULT '', " +
+				"serverUser VARCHAR(255) DEFAULT '', " +
+				"serverDatabase VARCHAR(255) DEFAULT ''" +
+				")";
+			query.SetCommand(sqlCommand);
+			query.Execute();
+			
+			sqlCommand = "INSERT INTO Settings (" +
+				"[name], [localDatabase], [typeDatabase], [typeConnection], " +
+				"[server], [serverUser], [serverDatabase]) " +
+				"VALUES ('database', '" + DataConfig.localDatabase + "', '" 
+				+ DataConfig.typeDatabase +"', '" + DataConfig.typeConnection + "', '"
+				+ DataConfig.server + "', '" + DataConfig.serverUser + "', '" 
+				+ DataConfig.serverDatabase + "')";
+			query.SetCommand(sqlCommand);
+			query.Execute();
+
+		}
+	}
+}
